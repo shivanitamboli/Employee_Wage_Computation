b17c053904cdf4818ee90ffcf481eba1dbc619c5
shivanitamboli <88812889+shivanitamboli@users.noreply.github.com>
2021-09-03 23:56:37 +0530

    Add files via upload

---
+++ EmployeeWage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class EmployeeWage
+    {
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome To Employee Aage Computation Problem");
+
+            int IS_PART_Time= 1;
+            int IS_FULL_Time = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_Time)
+            {
+                empHrs = 4;
+                Console.WriteLine("Part_time");
+
+            }
+            else if (empCheck == IS_FULL_Time)
+            {
+                empHrs = 8;
+
+                Console.WriteLine("Full time");
+
+            }
+
+            else
+            {
+                empHrs = 0;
+
+
+                Console.WriteLine("absent");
+
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("emp wage:" + empWage);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
