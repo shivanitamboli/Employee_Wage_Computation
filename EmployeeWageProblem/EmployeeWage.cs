99d0506a5720ecfe7e2c745dcf5dbe60b2004f88
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-03 15:51:36 +0530

    s

---
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation.EmployeeWageProblem
+{
+    class EmployeeWage
+    {
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage");
+            Console.ReadKey();
+
+        }
+    }
+}
+
