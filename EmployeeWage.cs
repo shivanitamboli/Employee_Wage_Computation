0c0f24bd689842bd672b32ca5d1d842982bd20fe
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-03 16:07:36 +0530

    Added: Welcome Message

---
+++ EmployeeWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Aage Computation Problem");
+            Console.ReadKey();
+
+        }
+    }
+}
