e2afe6548b9009caeeaa227a1413c267402ef464
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-06 08:58:10 +0530

    month

--- Program.cs
+++ Program.cs
@@ -7,36 +7,39 @@ using System.Threading.Tasks;
 namespace EmployeeWageComputation
 {
     class Program
-    { 
-            //constant
-            static final int FULL_TIME = 1;
-            static final int EMP_WAGE_PER_HOUR = 20;
-            static final int PART_TIME = 2;
+    {
+       
+            public static final int FULL_TIME = 1;
+            public static final int PART_TIME = 2;
+            public static final int EMP_WAGE_PER_HOUR = 20;
+            public static final int NUM_OF_WORKING_DAY = 20;
+
             public static void main(String[] args)
             {
                 System.out.println("Welcome to Employee Wage");
-                int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-                int calcwage = 0;
-                int empHrs = 0;
-                switch (empCheck)
+                int empHrs = 0, calcwage = 0, totalEmpWage = 0;
+
+                //Computation
+                for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
                 {
-                    case PART_TIME:
-                        System.out.println("Empolyee is Present Part time");
-                        empHrs = 4;
-                        break;
-                    case FULL_TIME:
-                        System.out.println("Employee is Present Full time");
-                        empHrs = 8;
-                        break;
-                    default:
-                        System.out.println("Employee is Absent");
-                        empHrs = 0;
+                    int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+                    switch (empCheck)
+                    {
+                        case PART_TIME:
+                            empHrs = 4;
+                            break;
+                        case FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        default:
+                            empHrs = 0;
+                    }
+                    calcwage = empHrs * EMP_WAGE_PER_HOUR;
+                    totalEmpWage += calcwage;
+                    System.out.println("Employee daily Wage = " + calcwage + "Rs");
+
                 }
-                calcwage = empHrs * EMP_WAGE_PER_HOUR;
-                System.out.println("Employee daily Wage = " + calcwage + "Rs");
-
-
-
+                System.out.println("Total Emp Wage :" + totalEmpWage);
             }
         }
     }
